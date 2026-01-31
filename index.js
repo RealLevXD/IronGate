e3dec4202eb3168ee5667e939e69e57b51232c4f
RealLevXD <iamlevhere12@gmail.com>
2026-01-31 14:50:21 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ client.once('ready', () => {
   console.log(`✅ Logged in as ${client.user.tag}`);
 });
 
-client.login("MTQ2NzA1MTI5NTA1ODgyNTI2Ng.GYRqlR.dGKcWmYxCKzRqvEO266NE7rrIVVP1NTeoKsgp8");
+client.login(process.env.TOKEN);
